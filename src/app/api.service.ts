75941eabe9366ae8dc5103f32edfc2197d295c81
Andreas.Vu <andreas.loyning.vu@gmail.com>
2020-03-05 15:16:35 +0100

    added function for adding and getting cards/deck

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -6,10 +6,29 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class ApiService {
   private path = 'http://127.0.0.1:8000/'
-  private header = {
+
+  private tokenHeader = {
     headers: new HttpHeaders({
       'Content-Type': 'application/x-www-form-urlencoded',
       'accept': 'application/json'
+    })
+  }
+
+  private putHeader(token: string) {
+    return (
+      {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+          'accept': 'application/json',
+          'Authorization': `Bearer ${token}`
+        })
+      }
+    )
+  }
+
+  private getHeader = {
+    headers: new HttpHeaders({
+      'accept': 'application/json',
     })
   }
 
@@ -18,12 +37,68 @@ export class ApiService {
   getToken<Observable>(username: string, password: string) {
     return this.http.post<Token>(this.path + 'token/', 
     `grant_type=&username=${username}&password=${password}&scope=&client_id=&client_secret=`,
-    this.header
-    )
+    this.tokenHeader
+    );
+  }
+
+  getDecks<Observable>() {
+    return this.http.get<Array<Deck>>(this.path + 'deck/alldecks/',
+      this.getHeader
+    );
+  }
+
+  getDeck<Observable>(id: number) {
+    return this.http.get<Deck>(this.path + `deck/${id}`,
+      this.getHeader
+    );
+  }
+
+  getCard<Observable>(id: number) {
+    return this.http.get<Card>(this.path + `card/${id}`,
+      this.getHeader
+    );
+  }
+
+  putCard<Observable>(card: CardPutt, token: string) {
+    return this.http.put<Card>(this.path + `card/`,
+      card,
+      this.putHeader(token)
+    );
+  }
+
+  putDeck<Observable>(deck: DeckPutt, token: string) {
+    return this.http.put<DeckPutt>(this.path + `deck/`,
+      deck,
+      this.putHeader(token)
+    );
   }
 }
 
 export class Token {
   access_token: string
   token_type: string
+}
+
+export class Deck {
+  id: number;
+  title: string;
+  description: string;
+  cards: Array<Card>;
+}
+
+export class DeckPutt {
+  title: string;
+  description: string;
+  cards: Array<number>;
+}
+
+export class Card {
+  id: number;
+  text: string;
+  blanks: number;
+}
+
+export class CardPutt {
+  text: string;
+  blanks: number;
 }
