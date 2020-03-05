b9d17d3ba195e77f87791ad6373cc8e2f3556623
Andreas.Vu <andreas.loyning.vu@gmail.com>
2020-03-05 15:39:55 +0100

    Cleaning

--- src/app/logg-inn/logg-inn.component.ts
+++ src/app/logg-inn/logg-inn.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { ApiService, Token } from '../api.service';
-import { HttpClientModule, HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { tokenName } from '@angular/compiler';
 
 @Component({
   selector: 'app-logg-inn',
@@ -11,27 +9,25 @@ import { tokenName } from '@angular/compiler';
   styleUrls: ['./logg-inn.component.css']
 })
 export class LoggInnComponent implements OnInit {
-  loginForm;
-  apiService;
+  loginForm: FormGroup;
   token: Observable<Token>;
 
   constructor(
     private formBuilder: FormBuilder,
-    private http: HttpClient
+    private api: ApiService,
   ) {
     this.loginForm = this.formBuilder.group({
       username: '',
       password: ''
     });
 
-    this.apiService = new ApiService(http);
    }
 
   ngOnInit(): void {
   }
 
   async onSubmit(data: FormData) {
-    this.token = this.apiService.getToken(data['username'], data['password']);
+    this.token = this.api.getToken(data['username'], data['password']);
     this.token.subscribe(data => {
       console.log(data.access_token);
     });
